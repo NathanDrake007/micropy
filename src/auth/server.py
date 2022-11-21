2c0b93e248524508ee49ef29b6cc19ecd6d98781
NathanDrake007 <sakthi.nathan@zohocorp.com>
2022-11-21 12:44:35 +0530

    Auth service

---
+++ src/auth/server.py
@@ -0,0 +1,60 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL()
+
+#config
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+@server.route("/login",methods=["POST"])
+def login():
+    auth = request.authorization
+    if not auth:
+        return "missing credentials", 401
+    
+    # check db for username and password
+    curr = mysql.connection.cursor()
+    res = curr.execute("SELECT email,password FROM user WHERE email=%s", (auth.username,))
+
+    if res > 0:
+        user_row = curr.fetchone()
+        email,password = user_row
+        if email != auth.username and password != auth.password:
+            return "Invalid credentials", 401
+        else:
+            return createJWT(auth.username,os.environ.get("JWT_SECRET"),True)
+    else:
+        return "Invalid credentials", 401
+
+@server.route("/validate",method=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+    if not encoded_jwt:
+        return "Missing credentials", 401
+    token = encoded_jwt.split(" ")[1]
+    try:
+        decoded = jwt.decode(token,os.environ.get("JWT_SECRET"),algorithms=["HS256"])
+    except:
+        return "Not authorized", 403
+    return decoded, 200
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timedzone.utc)+ datetime.timedelta(days=1),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0",port=5000)
